1c58f7438c3b35126bbdf608a8ab063509aaba47
kiransahu-dev <sahukiran98.ks@gmail.com>
2023-06-05 09:51:27 +0530

    updated route

--- src/constants/route.js
+++ src/constants/route.js
@@ -1,7 +1,7 @@
 const APP_PATH = '/';
 
 export const routePath = {
-    home: `${APP_PATH}/`,
-    category: `${APP_PATH}/category`,
-    invalid: `${APP_PATH}/*`,
+    home: `${APP_PATH}`,
+    category: `${APP_PATH}category`,
+    invalid: `${APP_PATH}*`,
 }

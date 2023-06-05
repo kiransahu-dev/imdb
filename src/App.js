6e153f3a77ffac6ee816dd610fe0a55cefa1e6d4
kiransahu-dev <sahukiran98.ks@gmail.com>
2023-06-05 09:23:41 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { routePath } from './constants/route';
+import CategoryMovies from './pages/CategoryMovies';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={routePath.home} element={<Home />} />
+        <Route path={routePath.category} element={<CategoryMovies />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
